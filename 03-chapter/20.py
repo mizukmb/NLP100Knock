36b9c697737fa7d046a31989cde2ddea0c1ad70f
mizukmb <mizukmb6@gmail.com>
2015-11-05 14:42:43 +0900

    use mymodule in 20.py

--- 03-chapter/20.py
+++ 03-chapter/20.py
@@ -1,15 +1,3 @@
-# coding: utf-8
+import mymodule as m
 
-import gzip
-import json
-
-def get_article_for_UK():
-    text=""
-    for line in gzip.open('../data/jawiki-country.json.gz'):
-        data = json.loads(line)
-        if u'イギリス' in data['title']:
-            text = data['text']
-    return text
-
-t = get_article_for_UK()
-print t.encode('utf-8')
+print m.get_article_about_UK().encode('utf-8')
