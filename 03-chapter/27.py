38a057936d564ea451d4882a8a25b21f3e04de94
mizukmb <mizukmb6@gmail.com>
2015-11-24 18:13:59 +0900

    fix 27

--- 03-chapter/27.py
+++ 03-chapter/27.py
@@ -8,10 +8,13 @@ sys.setdefaultencoding("utf-8")
 text = m.get_article_about_UK()
 base_info_text = re.search("{{基礎情報 国\n(.+\n)*?}}", text.encode('utf-8'))
 dic = {}
-reg = re.compile("('+|\[\[|\]\]|{{|}})")
+reg_emphasis = re.compile("'+")
+reg_internal = re.compile("\[\[(?!.+:)(.*\|)?(.*?)\]\]")
 
 for line in base_info_text.group().split("\n"):
     reg_list = re.search("\|(.+?) = (.+?)(\Z|<)", line.encode('utf-8'))
     if reg_list:
-        dic[reg_list.group(1)] = reg_list.group(2)
-        print reg.sub("", reg_list.group(1)), reg.sub("", reg_list.group(2))
+        value = reg_internal.search(reg_list.group(2))
+        value = reg_emphasis.sub("", value.group(2)) if value else reg_emphasis.sub("", reg_list.group(2))
+        dic[reg_list.group(1)] = value
+        print reg_list.group(1), value
