d4c79ef7bf7d10f09ae472cdbd5c216b78d274bf
mizukmb <mizukmb6@gmail.com>
2016-01-30 18:58:36 +0900

    fix 27.py

--- 03-chapter/27.py
+++ 03-chapter/27.py
@@ -9,12 +9,12 @@ text = m.get_article_about_UK()
 base_info_text = re.search("{{基礎情報 国\n(.+\n)*?}}", text.encode('utf-8'))
 dic = {}
 reg_emphasis = re.compile("'+")
-reg_internal = re.compile("\[\[(?!.+:)(.*\|)?(.*?)\]\]")
+reg_internal = re.compile("\[\[|\]\]")
 
 for line in base_info_text.group().split("\n"):
-    reg_list = re.search("\|(.+?) = (.+?)(\Z|<)", line.encode('utf-8'))
+    reg_list = re.search("\|(.+) = (.+)", line.encode('utf-8'))
     if reg_list:
-        value = reg_internal.search(reg_list.group(2))
-        value = reg_emphasis.sub("", value.group(2)) if value else reg_emphasis.sub("", reg_list.group(2))
+        value = reg_emphasis.sub("", reg_list.group(2).split("<ref>")[0])
+        value = reg_internal.sub("", value)
         dic[reg_list.group(1)] = value
         print reg_list.group(1), value
