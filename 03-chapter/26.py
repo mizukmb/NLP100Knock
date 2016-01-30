22f8b0b608eb9edc40f59cabe3bf3f35d754138e
mizukmb <mizukmb6@gmail.com>
2016-01-30 18:32:08 +0900

    fix 26.py

--- 03-chapter/26.py
+++ 03-chapter/26.py
@@ -8,10 +8,10 @@ sys.setdefaultencoding("utf-8")
 text = m.get_article_about_UK()
 base_info_text = re.search("{{基礎情報 国\n(.+\n)*?}}", text.encode('utf-8'))
 dic = {}
-reg = re.compile("'+")
+reg = re.compile("''+")
 
 for line in base_info_text.group().split("\n"):
-    reg_list = re.search("\|(.+?) = (.+?)(\Z|<)", line.encode('utf-8'))
+    reg_list = re.search("\|(.+) = (.+)", line.encode('utf-8'))
     if reg_list:
-        dic[reg_list.group(1)] = reg_list.group(2)
-        print reg.sub("", reg_list.group(1)), reg.sub("", reg_list.group(2))
+        dic[reg_list.group(1)] = reg.sub("", reg_list.group(2).split("<ref>")[0])
+        print reg_list.group(1) , dic[reg_list.group(1)]
