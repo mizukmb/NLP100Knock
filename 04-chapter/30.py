fa1b542cf5404af8d4a9c30412c860f52218ea19
mizukmb <mizukmb6@gmail.com>
2016-02-07 14:04:33 +0900

    use mymodule in 30.py

--- 04-chapter/30.py
+++ 04-chapter/30.py
@@ -1,27 +1,11 @@
 # coding: utf-8
 
-from janome.tokenizer import Tokenizer
-
-def get_morphs(string):
-    t = Tokenizer()
-    dicts=[]
-    for token in t.tokenize(unicode(string, 'utf-8')):
-        dic = {}
-        token_list = str(token).replace("	", ",").split(",")
-        dic["surface"] = token_list[0]
-        dic["base"] = token_list[6]
-        dic["pos"] = token_list[1]
-        dic["pos1"] = token_list[2]
-
-        print dic["surface"], dic["base"], dic["pos"], dic["pos1"]
-        dicts.append(dic)
-
-    return dicts
+import mymodule as m
 
 text = open("../data/neko.txt").read()
 
 dict_list = []
 for string in text.split():
     c_string = string.replace("　", "")
-    dict_list.append(get_morphs(c_string))
+    dict_list.append(m.get_morphs(c_string))
 
