d613fc92c2554aacf0d4071c9808db23cb6567e2
mizukmb <mizukmb6@gmail.com>
2016-02-07 14:22:34 +0900

    finish 31

---
+++ 04-chapter/31.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+import mymodule as m
+
+text = open("../data/neko.txt").read()
+
+dict_list = []
+for string in text.split():
+    c_string = string.replace("　", "")
+    for morph in m.get_morphs(c_string):
+        if morph["pos"] == "動詞":
+            print morph["surface"]
+            dict_list.append(morph["surface"])
+
